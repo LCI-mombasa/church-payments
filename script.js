94793f01551071249591444f27f34a5f76a5b143
LCI-mombasa <arthurjohndavid95@gmail.com>
2025-12-04 17:49:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,9 @@ function openMpesa() {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
   if (/android/i.test(userAgent)) {
-    // Try opening M-Pesa app on Android
-    window.location = "mpesa://";
-    setTimeout(function() {
-      window.location = "https://play.google.com/store/apps/details?id=com.safaricom.mpesa";
-    }, 2000);
+    window.location = "https://play.google.com/store/apps/details?id=com.safaricom.mpesa";
   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    // Try opening M-Pesa app on iOS
-    window.location = "mpesa://";
-    setTimeout(function() {
-      window.location = "https://apps.apple.com/ke/app/m-pesa/id1581016672";
-    }, 2000);
+    window.location = "https://apps.apple.com/ke/app/m-pesa/id1581016672";
   } else {
     alert("Please use your phone to open M-Pesa.");
   }
